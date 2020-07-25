1001ff8c9ffccf6beac7406952aa5ffb170fa031
Ankan Mondal <mailtoankan2001@gmail.com>
2020-07-25 21:12:01 +0530

    initial commit

---
+++ public/js/signup.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const signup = async (name ,email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:3000/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
+      }
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Signed up successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+    console.log(res);
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout'
+    });
+    if ((res.data.status = 'success')) location.reload(true);
+  } catch (err) {
+    console.log(err.response);
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
