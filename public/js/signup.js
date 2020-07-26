b7a705f6b2bc4d3b13a1c6af5b37fa8df58506e5
Ankan Mondal <mailtoankan2001@gmail.com>
2020-07-26 13:07:31 +0530

    prepared app for our deployment

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,7 +6,8 @@ export const signup = async (name ,email, password, passwordConfirm) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/signup',
+      // url: 'http://127.0.0.1:3000/api/v1/users/signup',
+      url: '/api/v1/users/signup',
       data: {
         name,
         email,
@@ -21,7 +22,7 @@ export const signup = async (name ,email, password, passwordConfirm) => {
         location.assign('/');
       }, 1500);
     }
-    console.log(res);
+    // console.log(res);
   } catch (err) {
     showAlert('error', err.response.data.message);
   }
@@ -31,11 +32,12 @@ export const logout = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout'
+      // url: 'http://127.0.0.1:3000/api/v1/users/logout'
+      url: '/api/v1/users/logout'
     });
     if ((res.data.status = 'success')) location.reload(true);
   } catch (err) {
-    console.log(err.response);
+    // console.log(err.response);
     showAlert('error', 'Error logging out! Try again.');
   }
 };
